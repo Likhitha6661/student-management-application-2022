76f95f89451ac36bf90f88f92edd3bf24f6f5fb1
Likhitha <likithachowdary.rise@gmail.com>
2022-05-02 16:02:13 +0530

    Message

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main{
+public static void main(String args[])
+{
+    if(args.length>0){
+      System.out.println(args[0]);
+    }
+    else{
+        System.out.println("No arguments is given.");
+    }
+}
+}
