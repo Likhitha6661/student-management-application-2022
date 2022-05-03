953564e8de715f6c90beb6dabf01efcb16215ff4
Srivalli P <54148181+psrivalli2001@users.noreply.github.com>
2022-05-03 22:40:18 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,9 +3,7 @@ class SimpleCommandLine
     public static void main(String args[])
     {
         if(args.length>0){
-            for(int i=0;i<args.length;i++){
-                System.out.println(args[i]);
-            }
+                System.out.println(args[0]);
         }
         else
             System.out.print("No Arguments Given");
