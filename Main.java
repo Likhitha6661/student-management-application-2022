fc366b2382c86eb3c6fb6bb637312833fa00d6e7
Srivalli_P <psrivalli2001@gmail.com>
2022-05-03 21:46:23 +0530

    Implemented a simple java console program

---
+++ Main.java
@@ -0,0 +1,13 @@
+class SimpleCommandLine
+{
+    public static void main(String args[])
+    {
+        if(args.length>0){
+            for(int i=0;i<args.length;i++){
+                System.out.println(args[i]);
+            }
+        }
+        else
+            System.out.print("No Arguments Given");
+    }
+}
